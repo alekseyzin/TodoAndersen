25d757db75aa5ac76257bcda48ca5e213ad03b76
Aleksey <aleksey.zin@gmail.com>
2020-08-12 17:52:15 +0300

    add ability to select one of the existing items

--- model.js
+++ model.js
@@ -1,90 +1,89 @@
 export class Todo {
 
-    isTodoInputValid (todoItemText) {
-        const todoItemTextVal = todoItemText.value
-        if (todoItemTextVal.trim().length <= 0) {
-            todoItemText.classList.add('invalid')
+    isDataEmpty (dateInput) {
+        if (!dateInput.value) {
+            dateInput.classList.add('invalid')
+            return  true
+        } else {
+            dateInput.classList.remove('invalid')
             return false
-        } else {
-            todoItemText.classList.remove('invalid')
+        }
+    }
+
+    isDataPeriodInvalid (startDateInput, endDateInput) {
+        const timeForTodo = new Date(endDateInput.value) - new Date(startDateInput.value)
+
+        if (!timeForTodo || timeForTodo <= 0) {
+            startDateInput.classList.add('invalid')
+            endDateInput.classList.add('invalid')
             return true
+        } else {
+            startDateInput.classList.remove('invalid')
+            endDateInput.classList.remove('invalid')
+            return false
         }
     }
 
-    isFormModalValid (todoItemText, dateCreate, dateEnd) {
-        const [todoItemTextVal, dateCreateVal, dateEndVal] = [todoItemText.value, dateCreate.value, dateEnd.value]
-        const timeForTodo = new Date(dateEndVal) - new Date(dateCreateVal);
-
-        let isError = false
-        if (!dateCreateVal) {
-            dateCreate.classList.add('invalid')
-            isError = true
+    isTodoInputEmpty (todoInput) {
+        if (todoInput.value.trim().length <= 0) {
+            todoInput.classList.add('invalid')
+            return true
         } else {
-            dateCreate.classList.remove('invalid')
+            todoInput.classList.remove('invalid')
         }
-
-        if (!dateEndVal) {
-            dateEnd.classList.add('invalid')
-            isError = true
-        } else {
-            dateEnd.classList.remove('invalid')
-        }
-
-        if (!isError) {
-            if (!timeForTodo || timeForTodo <= 0) {
-                dateEnd.classList.add('invalid')
-                dateCreate.classList.add('invalid')
-                isError = true
-            } else {
-                dateCreate.classList.remove('invalid')
-                dateEnd.classList.remove('invalid')
-            }
-        }
-
-        if (todoItemTextVal.trim().length <= 0) {
-            todoItemText.classList.add('invalid')
-            isError = true
-        } else {
-            todoItemText.classList.remove('invalid')
-        }
-
-        return isError ? false : true
     }
 
-    todoToHTML (todoItemText, dateCreate, dateEnd) {
+    isFormModalValid(todoInput, startDateInput, endDateInput) {
+        const errorArray = []
+
+        errorArray.push(this.isDataEmpty(startDateInput))
+        errorArray.push(this.isDataEmpty(endDateInput))
+        if (!errorArray.includes(true)) {
+            errorArray.push(this.isDataPeriodInvalid(startDateInput, endDateInput))
+        }
+        errorArray.push(this.isTodoInputEmpty(todoInput))
+        return errorArray.includes(true) ? false : true
+    }
+
+    todoToHTML(todoItemText, dateCreate, dateEnd) {
         const todoListBlock = document.querySelector('.todo-list')
         const li = document.createElement('li')
+
         li.innerHTML = `
-            <span class="todo-text">${todoItemText}</span>
-            <div class="data-holder">
-                <span>Start: ${dateCreate}</span>
-                <span>Finish: ${dateEnd}</span>
+            <input type="checkbox">
+            <div>
+                <span class="todo-text">${todoItemText}</span>
+                <div class="data-holder">
+                    <span>Start: ${dateCreate}</span>
+                    <span>Finish: ${dateEnd}</span>
+                </div>
             </div>
         `
         todoListBlock.appendChild(li)
     }
 
-    getDateObject () {
-        const dateNow = new Date()
-        const dateCreate = dateNow.toLocaleDateString()
-        const dateClone = new Date(dateNow)
-        const dateEnd = new Date(dateClone.setDate(dateNow.getDate() + 1)).toLocaleDateString()
+    getDateObject() {
+        const dateCreate = new Date().toLocaleDateString()
+        const dateEnd = new Date(new Date().setDate(new Date().getDate() + 1)).toLocaleDateString()
+
         return {dateCreate, dateEnd}
     }
 
-    addTodoItem (todoItemText) {
+    addTodoItem(todoItemText) {
         const todoItemTextVal = todoItemText.value
-        if (this.isTodoInputValid (todoItemText)) {
+
+        if (!this.isTodoInputEmpty(todoItemText)) {
             const {dateCreate, dateEnd} = this.getDateObject()
             this.todoToHTML(todoItemTextVal, dateCreate, dateEnd)
         }
     }
 
-    isAddTodoWithOptions (todoItemText, dateCreate, dateEnd) {
+    isAddTodoWithOptions(todoItemText, dateCreate, dateEnd) {
         let [todoItemTextVal, dateCreateVal, dateEndVal] = [todoItemText.value, dateCreate.value, dateEnd.value]
+
         dateCreateVal = dateCreateVal.split("-").reverse().join(".")
         dateEndVal = dateEndVal.split("-").reverse().join(".")
-        if (this.isFormModalValid (todoItemText, dateCreate, dateEnd)) {
+        if (this.isFormModalValid(todoItemText, dateCreate, dateEnd)) {
             this.todoToHTML(todoItemTextVal, dateCreateVal, dateEndVal)
             todoItemText.value = ''
             dateCreate.value = ''
@@ -95,4 +94,5 @@ export class Todo {
         }
 
     }
+
 }
--- script.js
+++ script.js
@@ -4,26 +4,29 @@ const todo = new Todo()
 
 function toggleModal () {
     const modal = document.querySelector(".modal-holder")
+
     modal.classList.toggle('open')
 }
 
 function  saveModalData () {
     const validation = todo.isAddTodoWithOptions(modalInputText, modalInputStartDate, modalInputEndDate)
+
     if (validation) toggleModal()
 }
 
 function closeModalWhenPressEscape (e) {
     if (e.key === "Escape") {
         const modal = document.querySelector(".modal-holder")
+
         modal.classList.remove('open')
     }
 }
 
 function hasNotSpecialSymbols (e) {
-    var regex = new RegExp("^(?=.*[A-Za-z0-9])[A-Za-z0-9 _]*$");
+    const regex = new RegExp("^(?=.*[A-Za-z0-9 _])[A-Za-z0-9 _]*$");
+
     if (!regex.test(e.key)) {
         e.preventDefault()
-        return false
     }
 }
 
@@ -34,6 +37,14 @@ function sendTodoDataWhenPressEnter (e) {
     }
 }
 
+function findAndMarkTodo (e) {
+    if (e.target.type === "checkbox") {
+        const textBlock = e.target.parentElement.querySelector('.todo-text')
+
+        textBlock.classList.toggle('mark')
+    }
+}
+
 addTodo.addEventListener('keydown', hasNotSpecialSymbols)
 addTodo.addEventListener('keydown', sendTodoDataWhenPressEnter)
 modalInputText.addEventListener('keydown', hasNotSpecialSymbols)
@@ -41,6 +52,7 @@ document.addEventListener('keydown', closeModalWhenPressEscape)
 openModalButton.addEventListener('click', toggleModal)
 closeModal.addEventListener('click', toggleModal)
 saveModal.addEventListener('click', saveModalData)
+todoList.addEventListener('click', findAndMarkTodo)
 
 
 
